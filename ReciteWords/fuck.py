6c6a830f916e73020cfe9b5ecc48d49c5e408ea9
xiaoqiang <3160101819@zju.edu.cn>
2019-07-16 20:17:05 +0800

    1st stage

--- ReciteWords/fuck.py
+++ ReciteWords/fuck.py
@@ -1,12 +1,12 @@
-from BeautifulSoup import BeautifulSoup
+# from BeautifulSoup import BeautifulSoup
 from markdown import markdown
 
 def main():
 	file_name = 'static/md/recite_words.md'
 	output_name = 'static/html/recite_words.html'
-	with open(file_name, 'r') as f:
+	with open(file_name, 'r', encoding = 'utf-8') as f:
 		html = markdown(f.read())
-		with open(output_name, 'w') as o:
+		with open(output_name, 'w', encoding = 'utf-8') as o:
 			o.write(html)
 
 if __name__ == '__main__':
